81996b2b4929c2ea568f506181211ac7cfdc7c9f
bliuag <bliuag@connect.ust.hk>
2016-02-18 23:39:53 +0800

    start

--- testFLTK.cpp
+++ testFLTK.cpp
@@ -1,8 +1,6 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
-#include <FL/Fl_Button.H>
-#include <FL/Fl_Check_Button.H>
+
 
 int main(int argc, char **argv) {
     Fl_Window *window = new Fl_Window(600, 360);
